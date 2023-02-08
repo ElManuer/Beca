3aed91851876404aae5a5cd73a850169a02fd59f
Second Son <118853502+ElManuer@users.noreply.github.com>
2023-02-07 20:36:14 -0400

    Ejercico numero 2

---
+++ Beca.py
@@ -0,0 +1,21 @@
+import random
+
+
+list = [random.randrange(1,6),random.randrange(1,6),random.randrange(1,6)]
+print(list)
+
+if list[0] == list[1]:
+    print(list[0] * 2)
+
+elif list[1] == list[2]:
+    print(list[1] * 2)
+
+elif list[0] == list[2]:
+    print(list[0] * 2)
+
+elif list[0] != list[1] != list[2]:
+    print(max(list))
+
+elif list[0] == list[1] and list[1] == list[2] and list[0] == list[2]:
+    print(list[0] * 3)
+
