5bfb793d8826d8d36caed6bb7e1e7db38c20173d
Second Son <118853502+ElManuer@users.noreply.github.com>
2023-02-07 20:48:08 -0400

    Update Beca.py

--- Beca.py
+++ Beca.py
@@ -6,14 +6,14 @@ print(list)
 if list[0] == list[1] and list[1] == list[2] and list[0] == list[2]:
     print(list[0] * 3)
 
-elif list[0] != list[1] != list[2]:
-    print(max(list))
+elif list[0] == list[2]:
+    print(list[0] * 2)
 
 elif list[0] == list[1]:
     print(list[0] * 2)
 
-elif list[0] == list[2]:
-    print(list[0] * 2)
-
 elif list[1] == list[2]:
-    print(list[0] * 2)
+    print(list[1] * 2)
+
+elif list[0] != list[1] != list[2]:
+    print(max(list))
